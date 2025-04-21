b2a6ac2bbc812b783a13f1aa85d1dd4035068af1
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-04-22 03:22:19 +0500

    add footer components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+export default function Footer() {
+  return (
+    <footer className="py-6 text-center text-gray-500 text-sm bg-[#0a0f2b]">
+      © 2025 Qasim Afzaal. All rights reserved.
+    </footer>
+  );
+}
